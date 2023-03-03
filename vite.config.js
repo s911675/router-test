e99e5832fb0f6bbfc5c1c534df8b12c34c7f0e82
노봉균 <bgnoh@ydns.co.kr>
2023-03-03 18:56:06 +0900

    userFetch 함수 추가

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import {fileURLToPath, URL} from "url";
+import { fileURLToPath, URL } from 'url';
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite';
 
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
     plugins: [vue(), eslintPlugin()],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
 
@@ -30,6 +29,5 @@ export default defineConfig({
         assetsInlineLimit: 4096, // 0 : off
         cssCodeSplit: true,
         sourcemap: false, // true, inline, hidden
-
     },
-})
+});
